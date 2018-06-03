75b1cf95bd978605560764fd4b0f831954779446
James Hillyerd <james@hillyerd.com>
2018-06-03 13:43:14 -0700

    webui: Remove unused mailbox handlers, rename routes #105
    - Many routes not needed by Elm UI.
    - `/serve/mailbox/*` becomes `/serve/m/*`.

--- pkg/webui/routes.go
+++ pkg/webui/routes.go
@@ -17,23 +17,15 @@ func NewRouter() *mux.Router {
 		web.Handler(RootMonitorMailbox)).Name("RootMonitorMailbox").Methods("GET")
 	r.Path("/serve/status").Handler(
 		web.Handler(RootStatus)).Name("RootStatus").Methods("GET")
-	r.Path("/serve/link/{name}/{id}").Handler(
-		web.Handler(MailboxLink)).Name("MailboxLink").Methods("GET")
-	r.Path("/serve/mailbox").Handler(
-		web.Handler(MailboxIndex)).Name("MailboxIndex").Methods("GET")
-	r.Path("/serve/mailbox/{name}").Handler(
-		web.Handler(MailboxList)).Name("MailboxList").Methods("GET")
-	r.Path("/serve/mailbox/{name}/{id}").Handler(
+	r.Path("/serve/m/{name}/{id}").Handler(
 		web.Handler(MailboxShow)).Name("MailboxShow").Methods("GET")
-	r.Path("/serve/mailbox/{name}/{id}/html").Handler(
+	r.Path("/serve/m/{name}/{id}/html").Handler(
 		web.Handler(MailboxHTML)).Name("MailboxHtml").Methods("GET")
-	r.Path("/serve/mailbox/{name}/{id}/source").Handler(
+	r.Path("/serve/m/{name}/{id}/source").Handler(
 		web.Handler(MailboxSource)).Name("MailboxSource").Methods("GET")
-	r.Path("/serve/mailbox/dattach/{name}/{id}/{num}/{file}").Handler(
+	r.Path("/serve/m/dattach/{name}/{id}/{num}/{file}").Handler(
 		web.Handler(MailboxDownloadAttach)).Name("MailboxDownloadAttach").Methods("GET")
-	r.Path("/serve/mailbox/vattach/{name}/{id}/{num}/{file}").Handler(
+	r.Path("/serve/m/vattach/{name}/{id}/{num}/{file}").Handler(
 		web.Handler(MailboxViewAttach)).Name("MailboxViewAttach").Methods("GET")
-	r.Path("/serve/{name}").Handler(
-		web.Handler(MailboxIndexFriendly)).Name("MailboxListFriendly").Methods("GET")
 	return r
 }
